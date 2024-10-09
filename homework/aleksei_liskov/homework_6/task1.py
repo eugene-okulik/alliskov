798c7b5aa400bc1999926023d3e49555a8d31142
Aleksei Liskov <liskov9@gmail.com>
2024-10-09 14:21:21 +0200

    added homework 6

---
+++ homework/aleksei_liskov/homework_6/task1.py
@@ -0,0 +1,21 @@
+text = ("Etiam tincidunt neque erat, quis molestie enim imperdiet vel. "
+        "Integer urna nisl, facilisis vitae semper at, dignissim vitae libero")
+punctuation_marks_one_symbol = (',', '.', '!', '?', ';')
+punctuation_marks_three_symbols = ('!!!', '...', '???')
+words = text.split()
+final_text = []
+
+for word in words:
+    if word.endswith(punctuation_marks_one_symbol):
+        punctuation_mark = word[:-2:-1]
+        final_word = word.rstrip(punctuation_mark) + 'ing' + punctuation_mark
+        final_text.append(final_word)
+    elif word.endswith(punctuation_marks_three_symbols):
+        punctuation_mark = word[:-4:-1]
+        final_word = word.rstrip(punctuation_mark) + 'ing' + punctuation_mark
+        final_text.append(final_word)
+    else:
+        final_word = word + 'ing'
+        final_text.append(final_word)
+
+print(' '.join(final_text))
