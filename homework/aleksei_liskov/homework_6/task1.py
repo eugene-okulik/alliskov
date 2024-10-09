7e959c981d6467ec774cc4b3203464a38ef6ad45
Aleksei Liskov <liskov9@gmail.com>
2024-10-09 14:53:06 +0200

    added join options to the task1

--- homework/aleksei_liskov/homework_6/task1.py
+++ homework/aleksei_liskov/homework_6/task1.py
@@ -8,14 +8,17 @@ final_text = []
 for word in words:
     if word.endswith(punctuation_marks_one_symbol):
         punctuation_mark = word[:-2:-1]
-        final_word = word.rstrip(punctuation_mark) + 'ing' + punctuation_mark
+        # final_word = word.rstrip(punctuation_mark) + 'ing' + punctuation_mark  # +
+        final_word = ''.join([word.strip(punctuation_mark), 'ing', punctuation_mark])  # join
         final_text.append(final_word)
     elif word.endswith(punctuation_marks_three_symbols):
         punctuation_mark = word[:-4:-1]
-        final_word = word.rstrip(punctuation_mark) + 'ing' + punctuation_mark
+        # final_word = word.rstrip(punctuation_mark) + 'ing' + punctuation_mark  # +
+        final_word = ''.join([word.rstrip(punctuation_mark), 'ing', punctuation_mark])  # join
         final_text.append(final_word)
     else:
-        final_word = word + 'ing'
+        # final_word = word + 'ing'  # +
+        final_word = ''.join([word, 'ing'])  # join
         final_text.append(final_word)
 
 print(' '.join(final_text))
